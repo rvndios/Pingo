d2b9b058df5e52486194ba1ee72d749ba44f2bd3
Aravind <aravind.s@versa-networks.com>
2023-09-06 14:09:23 +0530

    Demo app added

---
+++ DemoAPP/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DemoAPP
+//
+//  Created by Aravind on 06/09/23.
+//
+
+import UIKit
+import iPing
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        start()
+    }
+
+
+}
+extension ViewController {
+    func start() {
+        Pingo.ping(hosts: ["google.com", "cnn.com"]) { results in
+            print(results)
+        }
+    }
+}
+
